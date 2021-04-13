1314f6f0354b8fda84e09a7fca391fd72fab0ff1
siqixiang20 <xsiqi@live.unc.edu>
2021-04-13 18:59:39 -0400

    Correct isLeapYear method

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -18,7 +18,7 @@ public class LeapYear {
     /** Check if year is a Leap Year. */
         if (year % 400 == 0) {
             return true; 
-        } else if (year % 4 == 0) {
+        } else if (year % 4 == 0 && year % 100 != 0) {
             return true;
         } else {
             return false;
