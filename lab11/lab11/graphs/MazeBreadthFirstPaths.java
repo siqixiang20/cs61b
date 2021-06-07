f2ca75fd5e21b4c0fd7e4b210da75f050232f8ba
siqixiang20 <xsiqi@live.unc.edu>
2021-06-07 10:19:00 -0400

    Optional not finished up.

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,5 +1,11 @@
 package lab11.graphs;
 
+import java.awt.desktop.QuitEvent;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  *  @author Josh Hug
  */
@@ -9,21 +15,62 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int s;
+    private int t;
+    private boolean targetFound = false;
+    private Queue<Integer> fringe = new LinkedList<>();
+    private Maze maze;
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
         // Add more variables here!
+        maze = m;
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
-    private void bfs() {
-        // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+    private void bfs(int s) {
+        // Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        marked[s] = true;
+        announce();
+        fringe.add(s);
+
+        if (s == t) {
+            targetFound = true;
+        }
+        if (targetFound) {
+            return;
+        }
+
+        while (!fringe.isEmpty()) {
+            int v = fringe.poll();
+            for (int w: maze.adj(v)) {
+                if (!marked[w]) {
+                    fringe.add(w);
+                    marked[w] = true;
+                    announce();
+                    edgeTo[w] = v;
+                    distTo[w] = distTo[v] + 1;
+                    if (w == t) {
+                        targetFound = true;
+                    }
+                    if (targetFound) {
+                        return;
+                    }
+                }
+            }
+        }
+
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+        bfs(s);
     }
+
 }
 
